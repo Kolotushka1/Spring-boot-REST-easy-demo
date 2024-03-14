40d290df9e126c5540f4796ede8f1dd0c8d26103
Kolotushka1 <maksim.kolotov.050605@mail.ru>
2024-03-14 17:08:53 +0700

    Add ControllerAdvice
    Add ControllerAdvice

--- project/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ project/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -38,8 +38,6 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
 
-        // just index into the list ... keep it simple for now
-
         // check the studentID again list size
 
         if ((studentId >= theStudents.size()) || (studentId < 0)) {
@@ -47,41 +45,5 @@ public class StudentRestController {
         }
 
         return theStudents.get(studentId);
-    }
-
-    // Add an exception handler using @ExceptionHandler
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handlerException(StudentNotFoundException exc) {
-
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new  ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler ... to catch any exception (catch them all)
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new  ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
